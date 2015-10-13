823213b07bb14f794df8db6eb3d7abeb654d9a7a
Chang Liu <lllccc2009@gmail.com>
2015-10-13 14:14:48 -0400

    update scripts and labels

---
+++ data/xray/create_dataset_diagose.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import sys
+import os
+import shutil
+
+PATH = '/home/ycao/cli/xray_images/'
+
+src = ['LI', 'GH','CA', 'MI', 'AI', 'OT']
+src_file_list = ['LI.list', 'GH.list', 'CA.list', 'MI.list', 'AI.list', 'OT.list']
+label_file = ['xray_diagnose_train.txt', 'xray_diagnose_test.txt']
+label_tag = [0, 1, 2, 3, 4, 5]
+
+# create a folder that contains the images for training and testing
+# generate the label text files
+def create_dataset(src, src_file_list):
+
+	# generate the folder that contains for different labels 	
+	for list_src in src:
+		absolute_path = './' + list_src
+		if not os.path.exists(absolute_path):
+			os.mkdir(absolute_path)
+
+	# open the file that contains the lists
+	for file in src_file_list:
+		file_handler = open(file, "r+")
+		index = src_file_list.index(file)
+		tag = label_tag[index]
+
+		train_handler = open(label_file[0], "a+")
+		test_handler = open(label_file[1], "a+")
+
+		count = 0
+
+		# use the filelist to generate, we are sure that file exist in this folder
+		# so no judge for existance of the file
+		for line in file_handler:
+			file_name = PATH + line[1:-2]
+		 	shutil.copy(file_name, './' + str(src[index]))
+	
+			if count % 5 == 0:
+				test_handler.write(file_name + ' ' + str(tag) + '\n')
+			else:
+				train_handler.write(file_name + ' ' + str(tag) + '\n')
+			count += 1
+				
+		file_handler.close()
+		test_handler.close()
+		train_handler.close()
+
+if __name__ == "__main__":
+	create_dataset(src, src_file_list)	
--- data/xray/disorder.sh
+++ data/xray/disorder.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-SRC_TRAIN_FILE="xray_train.txt"
-SRC_TEST_FILE="xray_test.txt"
+SRC_TRAIN_FILE="xray_diagnose_train.txt"
+SRC_TEST_FILE="xray_diagnose_test.txt"
 TMP1_FILE="tmp1.txt"
 TMP2_FILE="tmp2.txt"
-OUT_TRAIN_FILE="xray_train_sort.txt"
-OUT_TEST_FILE="xray_test_sort.txt"
+OUT_TRAIN_FILE="xray_diagnose_train_sort.txt"
+OUT_TEST_FILE="xray_diagnose_test_sort.txt"
 
 # sorting 3 times for train.txt
 sort -R ${SRC_TRAIN_FILE} > ${TMP1_FILE}
