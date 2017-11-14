e69cac19a9dbf4918f6c21bda15f72540b791416
Chang Liu <lllccc2009@gmail.com>
2017-11-14 21:08:43 +0000

    add PythonDataLayer implementation

---
+++ multiclass-nihcc/examples/multiclass.py
@@ -0,0 +1,144 @@
+import sys 
+import os
+
+import numpy as np
+import os.path as osp
+import matplotlib.pyplot as plt
+
+from copy import copy
+
+plt.rcParams['figure.figsize'] = (6, 6)
+
+caffe_root = '../'  # this file is expected to be in {caffe_root}/examples
+sys.path.append(caffe_root + 'python')
+import caffe # If you get "No module named _caffe", either you have not built pycaffe or you have the wrong path.
+
+from caffe import layers as L, params as P # Shortcuts to define the net prototxt.
+
+sys.path.append("pycaffe/layers") # the datalayers we will use are in this directory.
+sys.path.append("pycaffe") # the tools file is in this folder
+
+import tools #this contains some tools that we need
+
+# set data root directory, e.g:
+pascal_root = osp.join(caffe_root, 'data/pascal/VOC2012')
+
+# these are the PASCAL classes, we'll need them later.
+classes = np.asarray(['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'])
+
+# make sure we have the caffenet weight downloaded.
+if not os.path.isfile(caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'):
+    print("Downloading pre-trained CaffeNet model...")
+
+# initialize caffe for gpu mode
+caffe.set_mode_gpu()
+caffe.set_device(0)
+
+# helper function for common structures
+def conv_relu(bottom, ks, nout, stride=1, pad=0, group=1):
+    conv = L.Convolution(bottom, kernel_size=ks, stride=stride,
+                                num_output=nout, pad=pad, group=group)
+    return conv, L.ReLU(conv, in_place=True)
+
+# another helper function
+def fc_relu(bottom, nout):
+    fc = L.InnerProduct(bottom, num_output=nout)
+    return fc, L.ReLU(fc, in_place=True)
+
+# yet another helper function
+def max_pool(bottom, ks, stride=1):
+    return L.Pooling(bottom, pool=P.Pooling.MAX, kernel_size=ks, stride=stride)
+
+# main netspec wrapper
+def caffenet_multilabel(data_layer_params, datalayer):
+    # setup the python data layer 
+    n = caffe.NetSpec()
+    n.data, n.label = L.Python(module = 'pascal_multilabel_datalayers', layer = datalayer, 
+                               ntop = 2, param_str=str(data_layer_params))
+
+    # the net itself
+    n.conv1, n.relu1 = conv_relu(n.data, 11, 96, stride=4)
+    n.pool1 = max_pool(n.relu1, 3, stride=2)
+    n.norm1 = L.LRN(n.pool1, local_size=5, alpha=1e-4, beta=0.75)
+    n.conv2, n.relu2 = conv_relu(n.norm1, 5, 256, pad=2, group=2)
+    n.pool2 = max_pool(n.relu2, 3, stride=2)
+    n.norm2 = L.LRN(n.pool2, local_size=5, alpha=1e-4, beta=0.75)
+    n.conv3, n.relu3 = conv_relu(n.norm2, 3, 384, pad=1)
+    n.conv4, n.relu4 = conv_relu(n.relu3, 3, 384, pad=1, group=2)
+    n.conv5, n.relu5 = conv_relu(n.relu4, 3, 256, pad=1, group=2)
+    n.pool5 = max_pool(n.relu5, 3, stride=2)
+    n.fc6, n.relu6 = fc_relu(n.pool5, 4096)
+    n.drop6 = L.Dropout(n.relu6, in_place=True)
+    n.fc7, n.relu7 = fc_relu(n.drop6, 4096)
+    n.drop7 = L.Dropout(n.relu7, in_place=True)
+    n.score = L.InnerProduct(n.drop7, num_output=20)
+    n.loss = L.SigmoidCrossEntropyLoss(n.score, n.label)
+    
+    return str(n.to_proto())
+
+
+workdir = './pascal_multilabel_with_datalayer'
+if not os.path.isdir(workdir):
+    os.makedirs(workdir)
+
+solverprototxt = tools.CaffeSolver(trainnet_prototxt_path = osp.join(workdir, "trainnet.prototxt"), testnet_prototxt_path = osp.join(workdir, "valnet.prototxt"))
+solverprototxt.sp['display'] = "1"
+solverprototxt.sp['base_lr'] = "0.0001"
+solverprototxt.write(osp.join(workdir, 'solver.prototxt'))
+
+# write train net.
+with open(osp.join(workdir, 'trainnet.prototxt'), 'w') as f:
+    # provide parameters to the data layer as a python dictionary. Easy as pie!
+    data_layer_params = dict(batch_size = 128, im_shape = [227, 227], split = 'train', pascal_root = pascal_root)
+    f.write(caffenet_multilabel(data_layer_params, 'PascalMultilabelDataLayerSync'))
+
+# write validation net.
+with open(osp.join(workdir, 'valnet.prototxt'), 'w') as f:
+    data_layer_params = dict(batch_size = 128, im_shape = [227, 227], split = 'val', pascal_root = pascal_root)
+    f.write(caffenet_multilabel(data_layer_params, 'PascalMultilabelDataLayerSync'))
+
+solver = caffe.SGDSolver(osp.join(workdir, 'solver.prototxt'))
+solver.net.copy_from(caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel')
+solver.test_nets[0].share_with(solver.net)
+solver.step(1)
+
+transformer = tools.SimpleTransformer() # This is simply to add back the bias, re-shuffle the color channels to RGB, and so on...
+image_index = 0 # First image in the batch.
+plt.figure()
+plt.imshow(transformer.deprocess(copy(solver.net.blobs['data'].data[image_index, ...])))
+gtlist = solver.net.blobs['label'].data[image_index, ...].astype(np.int)
+plt.title('GT: {}'.format(classes[np.where(gtlist)]))
+plt.axis('off');
+
+
+def hamming_distance(gt, est):
+    return sum([1 for (g, e) in zip(gt, est) if g == e]) / float(len(gt))
+
+def check_accuracy(net, num_batches, batch_size = 128):
+    acc = 0.0
+    for t in range(num_batches):
+        net.forward()
+        gts = net.blobs['label'].data
+        ests = net.blobs['score'].data > 0
+        for gt, est in zip(gts, ests): #for each ground truth and estimated label vector
+            acc += hamming_distance(gt, est)
+    return acc / (num_batches * batch_size)
+
+
+for itt in range(2):
+    solver.step(100)
+    print 'itt:{:3d}'.format((itt + 1) * 100), 'accuracy:{0:.4f}'.format(check_accuracy(solver.test_nets[0], 50))
+
+
+
+def check_baseline_accuracy(net, num_batches, batch_size = 128):
+    acc = 0.0
+    for t in range(num_batches):
+        net.forward()
+        gts = net.blobs['label'].data
+        ests = np.zeros((batch_size, len(gts)))
+        for gt, est in zip(gts, ests): #for each ground truth and estimated label vector
+            acc += hamming_distance(gt, est)
+    return acc / (num_batches * batch_size)
+
+print 'Baseline accuracy:{0:.4f}'.format(check_baseline_accuracy(solver.test_nets[0], 5823/128))
